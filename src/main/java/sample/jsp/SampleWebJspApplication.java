8b6c5789d517b0c77a187ee68b288c9a312ef098
asolanaruiz <alberto.solana.ruiz@gmail.com>
2014-05-13 10:36:18 -0700

    adding project files

---
+++ src/main/java/sample/jsp/SampleWebJspApplication.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class SampleWebJspApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SampleWebJspApplication.class, ServletContainerConfig.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(new Object[] {SampleWebJspApplication.class, ServletContainerConfig.class}, args);
+    }
+
+    @Bean
+    public TilesViewResolver tilesViewResolver() {
+        final TilesViewResolver tilesViewResolver = new TilesViewResolver();
+        tilesViewResolver.setOrder(0);
+        return tilesViewResolver;
+    }
+
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        final TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions("/WEB-INF/defs/pages-tiles-defs.xml", "/WEB-INF/defs/components-tiles-defs.xml");
+        return tilesConfigurer;
+    }
+
+}
